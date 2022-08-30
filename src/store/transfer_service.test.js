53607f3d14365f796b295282f495a89e5e9434cd
Primis-Labs <beels@primis.cc>
2022-08-30 11:56:09 +0800

    Modify unit tests

--- src/store/transfer_service.test.js
+++ src/store/transfer_service.test.js
@@ -1,27 +1,29 @@
+/* eslint-disable jest/valid-title */
 const  { initJsStore } = require("./idb_service");
 const { TransferService }  = require("./transfer_service");
 
-async function main(){
-   await initJsStore();
-   var from = '5Gb9AfeJnhedZN4H6xVEEixLmSatLmpZaacPLVMesZuRZxgr';
-   var obj = {
-     hash:'123456',
-     from:from,
-     to:'5Gb9AfeJnhedZN4H6xVEEixLmSatLmpZaacPLVMesZuRZxgr',
-     formatFrom:'gkKej1RjUhsfLCzVc4wHzd3mLCNet7EcvrC4n2FjU7fasSmzC',
-     balance:100,
-     symbols:'roc',
-     status:'1',
-     desc:'',
-     createTime:new Date()
-   }
 
-   var indexdb = new TransferService();
-   var r = indexdb.add(obj);
+describe(' transaction record storage ',()=>{
+  
+  test(' transaction storage test',async() => {
+    await initJsStore();
+    var from = '5Gb9AfeJnhedZN4H6xVEEixLmSatLmpZaacPLVMesZuRZxgr';
+    var obj = {
+      hash:'123456',
+      from:from,
+      to:'5Gb9AfeJnhedZN4H6xVEEixLmSatLmpZaacPLVMesZuRZxgr',
+      formatFrom:'gkKej1RjUhsfLCzVc4wHzd3mLCNet7EcvrC4n2FjU7fasSmzC',
+      balance:100,
+      symbols:'roc',
+      status:'1',
+      desc:'',
+      createTime:new Date()
+    }
+    var indexdb = new TransferService();
+    var r = indexdb.add(obj);
 
-   var query = indexdb.getTransfers(from);
-   console.log(`query db ${query}`);
+    var query = indexdb.getTransfers(from);
+    console.log(`query db ${query}`);
+  }) 
 
-}
-
-main()
+} )
